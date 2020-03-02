bd2fcf8f4398189c2390118fd78ab36ba17d2c29
Saurabh <55626361+Saurabh-FullStackDev@users.noreply.github.com>
2020-03-02 19:14:19 +0530

    CORS Changes

--- app.js
+++ app.js
@@ -23,7 +23,10 @@ const app = express();
 // API Security and Perforance
 const allowedOrigins = ['https://lean-frog.surge.sh/', 'http://localhost:3007'];
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://clever-bone.surge.sh',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}));
 
 app.use(helmet());
 
